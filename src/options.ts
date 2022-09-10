7f853f7505e1ff6f0d1c0f8c7bf523c26e6fa190
SukkaW <isukkaw@gmail.com>
2022-09-10 22:32:18 +0800

    refactor: replace joycon & jsonc-parser with get-tsconfig

--- src/options.ts
+++ src/options.ts
@@ -1,62 +1,13 @@
-import fs from 'fs';
-import JoyCon from 'joycon';
-import { parse } from 'jsonc-parser';
-
-const joycon = new JoyCon();
-
-joycon.addLoader({
-  test: /\.json$/,
-  load: async (file) => {
-    const content = await fs.promises.readFile(file, { encoding: 'utf-8' });
-    return parse(content);
-  }
-});
+import { getTsconfig } from 'get-tsconfig';
 
 export const getOptions = async (
   cwd: string,
   tsconfig?: string
-): Promise<{
-  importHelpers?: boolean,
-  esModuleInterop?: boolean,
-  experimentalDecorators?: boolean,
-  emitDecoratorMetadata?: boolean,
-  jsx?: 'preserve' | 'react' | 'react-jsx' | 'react-jsxdev' | 'react-native',
-  jsxFactory?: string,
-  jsxFragmentFactory?: string,
-  jsxImportSource?: string,
-  target?: string,
-  baseUrl?: string,
-  paths?: { [from: string]: [string] }
-}> => {
-  // joycon has its builtin-cache support
-  const { data, path } = await joycon.load([tsconfig || 'tsconfig.json', 'jsconfig.json'], cwd);
-  if (path && data) {
-    const {
-      importHelpers,
-      esModuleInterop,
-      experimentalDecorators,
-      emitDecoratorMetadata,
-      jsx,
-      jsxFactory,
-      jsxFragmentFactory,
-      jsxImportSource,
-      target,
-      baseUrl,
-      paths
-    } = data.compilerOptions || {};
-    return {
-      importHelpers,
-      esModuleInterop,
-      experimentalDecorators,
-      emitDecoratorMetadata,
-      jsx,
-      jsxFactory,
-      jsxFragmentFactory,
-      jsxImportSource,
-      target,
-      baseUrl,
-      paths
-    };
+) => {
+  let result = getTsconfig(cwd, tsconfig || 'tsconfig.json');
+  if (!result) {
+    result = getTsconfig(cwd, 'jsconfig.json');
   }
-  return {};
+
+  return result?.config.compilerOptions ?? {};
 };
