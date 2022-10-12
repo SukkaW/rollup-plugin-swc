73c26dbb7af7021f5823621f5a9973fd16a240a9
SukkaW <isukkaw@gmail.com>
2022-10-12 10:05:38 +0800

    chore: intial build using rollup3

--- tools/build.ts
+++ tools/build.ts
@@ -1,5 +1,7 @@
 import module from 'module';
-import { rollup } from 'rollup';
+
+import { rollup as rollup2 } from 'rollup';
+import { rollup as rollup3 } from 'rollup3';
 
 import dts from 'rollup-plugin-dts';
 import { swc, defineRollupSwcOption } from '../src/index';
@@ -11,24 +13,25 @@ async function main() {
   const external = ['@swc/core', ...deps, ...module.builtinModules];
 
   async function build() {
-    const bundle = await rollup({
+    const bundle = await rollup3({
       input: './src/index.ts',
       external,
       plugins: [swc(defineRollupSwcOption({
         jsc: {
           target: 'es2019'
         }
-      }))]
+      })) as any]
     });
 
     return Promise.all([
-      bundle.write({ file: './dist/index.js', format: 'cjs', exports: 'named' }),
-      bundle.write({ file: './dist/index.mjs', format: 'esm', exports: 'named' })
+      bundle.write({ file: './dist/index.js', format: 'cjs', exports: 'named', interop: 'auto' }),
+      bundle.write({ file: './dist/index.mjs', format: 'esm', exports: 'named', interop: 'auto' })
     ]);
   }
 
   async function createDtsFile() {
-    const bundle = await rollup({
+    // rollup-plugin-dts not yet support rollup 3, use rollup 2 to build the d.ts file
+    const bundle = await rollup2({
       input: './src/index.ts',
       external,
       plugins: [dts()]
