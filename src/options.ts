2247119dec494684b28dc66cafbd9c23c52e9806
citrus327 <citrus327@outlook.com>
2023-09-11 13:49:46 +0800

    feat: using ctx.warn instead of console.warn

--- src/options.ts
+++ src/options.ts
@@ -2,10 +2,12 @@ import { getTsconfig, parseTsconfig } from 'get-tsconfig';
 import path from 'path';
 
 import type { TsConfigJson } from 'get-tsconfig';
+import type { TransformPluginContext } from 'rollup';
 
 const cache = new Map<string, TsConfigJson.CompilerOptions>();
 
 export const getOptions = (
+  ctx: TransformPluginContext,
   cwd: string,
   tsconfig?: string
 ) => {
@@ -33,8 +35,7 @@ export const getOptions = (
   // Only fallback to `jsconfig.json` when tsconfig can not be resolved AND custom tsconfig filename is not provided
   if (!result && !tsconfig) {
     result = getTsconfig(cwd, 'jsconfig.json');
-  } else {
-    console.warn('[rollup-plugin-swc] tsconfig does not exists, fallback to jsconfig.json');
+    ctx.warn('tsconfig does not exists, fallback to jsconfig.json');
   }
 
   const compilerOptions = result?.config.compilerOptions ?? {};
