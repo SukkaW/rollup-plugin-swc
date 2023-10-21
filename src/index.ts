db69d78ba9d5587719723ab636e781ab3f209bae
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-10-21 06:03:19 -0700

    fix: pass correct option to swc (#54)

--- src/index.ts
+++ src/index.ts
@@ -117,8 +117,7 @@ function swc(options: PluginOptions = {}): RollupPlugin {
           externalHelpers: tsconfigOptions.importHelpers,
           parser: {
             syntax: isTypeScript ? 'typescript' : 'ecmascript',
-            tsx: isTypeScript ? isTsx : undefined,
-            jsx: !isTypeScript ? isJsx : undefined,
+            [isTypeScript?'tsx':'jsx']: isTypeScript ? isTsx : isJsx,
             decorators: tsconfigOptions.experimentalDecorators
           },
           transform: {
@@ -170,7 +169,10 @@ function swc(options: PluginOptions = {}): RollupPlugin {
 
     renderChunk(code: string) {
       if (options.minify || options.jsc?.minify?.mangle || options.jsc?.minify?.compress) {
-        return swcMinify(code, options.jsc?.minify);
+        return swcMinify(code, {
+          ...options.jsc?.minify,
+          module: true
+        });
       }
 
       return null;
