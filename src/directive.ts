404c30a5a82578c8fc75c1325124435e9810f142
Jiachi Liu <inbox@huozhi.im>
2023-12-01 02:59:28 +0100

    chore: replace `rollup-swc-preserve-directives` w/ `rollup-preserve-directives` (#60)

--- src/directive.ts
+++ src/directive.ts
@@ -1,4 +1,4 @@
 /**
  * preserveUseDirective is now a separate plugin, re-export to maintain the backward compatibility
  */
-export { default as preserveUseDirective } from 'rollup-swc-preserve-directives';
+export { default as preserveUseDirective } from 'rollup-preserve-directives';
